ee65b5afa467233ae9e23b18c089345d4eab7558
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-07-01 13:06:15 +0500

    header section completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Header from './components/Header';
+
 
 function App() {
   return (
     <div className="app">
-      <h1>HEllo</h1>
+      <Header/>
+
+    {/*App body/*}
+      {/*Sidebar*/}
+      {/*feed*/}
+      {/*widgets*/}
+
+
     </div>
   );
 }
